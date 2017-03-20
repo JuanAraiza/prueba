b9ba4b98e6e7fc492ecaac9089fba34e3803e606
Mac Juan Araiza <macbookretina@macbook-pro-de-mac-2.local>
2017-03-20 09:33:43 -0600

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Mac Juan Araiza on 20/03/17.
+//  Copyright © 2017 Juan Araiza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
